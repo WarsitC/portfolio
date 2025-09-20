b100835daae5381d92973e198bd5917c3a694bc8
Afsana Nahid Piya <afsananahidpiya@Afsanas-MacBook-Air.local>
2025-09-20 12:05:37 -0400

    Linked CSS and JS; Portfolio fully functional

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-console.log("Portfolio loaded successfully!");
+// Fade-in sections on scroll
+const sections = document.querySelectorAll("section");
 
-// Optional: you can add simple interactivity here later
+const showSection = () => {
+    const triggerBottom = window.innerHeight - 100;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if(sectionTop < triggerBottom){
+            section.classList.add("visible");
+        }
+    });
+}
+
+window.addEventListener("scroll", showSection);
+window.addEventListener("load", showSection);
